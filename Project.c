9d438fda6f1531afd93986d3a8daabc78a2f7253
Drashti <drashtiagrawal46@gmail.com>
2023-11-06 10:13:36 -0800

    Correct

--- Project.c
+++ Project.c
@@ -4,32 +4,31 @@
 #include<windows.h>
 #include<string.h>
 
-void gotop(int,int);
-void menu();
-void add();
-void search();
-void modify();
-void deleterec();
-void display();
+void gotop(int,int);      /*function to display output at x,y position */
+void menu();              
+void add();              /*Add Student Information*/
+void search();           /*Search Particular student */
+void modify();           /*Can modify detail of any individual student*/
+void deleterec();        /*Can delete the record the any student */
+void display();         /*Display the data of all students */
 struct student{
 
     char name[20];
-    char mobile[10];
-    int roll;
+    char mobile[10];     /*Mobile number of student */
+    int roll;           /*Roll number of student */
     char course[30];
     char branch[20];
 };
 int main(){
-   
+
 gotop(17,10);
 printf("***STUDENT RECORD MANAGEMENT SYSTEM***");
 gotop(21,15);
 printf("Press any key to continue");
-getch();
+getch();                               /*It will pause the execution of program until the user enters a character*/
 menu();
    
-   
-    return 0;
+return 0;
 }
 void gotop(int x,int y){
 
@@ -97,14 +96,14 @@ void menu(){
 }
 void add(){
 
-    FILE *f;
+    FILE *f;               
     struct student std;
     char another ='y';
-    system("cls");
+    system("cls");         /*To clear screen*/
 
     f = fopen("record.txt","ab+");
 
-   if(f==NULL){
+ if(f==NULL){
    gotop(12,5);
    printf("Error opening file");
    exit(1);
@@ -112,23 +111,23 @@ void add(){
    fflush(stdin);
    while(another=='y'){
       
-        gotop(12,3);
+       gotop(12,3);
        printf("<<==ADD RECORD==>>");
-        gotop(12,5);
+       gotop(12,5);
        printf("Enter details of student.");
-        gotop(12,7);
+       gotop(12,7);
        printf("Enter Name :");
-        gets(std.name);
+       gets(std.name);
        gotop(12,9);
        printf("Enter Mobile Number.");
-        gets(std.mobile);
-        gotop(12,11);
+       gets(std.mobile);
+       gotop(12,11);
        printf("Enter Roll NO.");
-        scanf("%d",&std.roll);
-        fflush(stdin);
-        gotop(12,13);
+       scanf("%d",&std.roll);
+       fflush(stdin);
+       gotop(12,13);
        printf("Enter Course .");
-        gets(std.course);
+       gets(std.course);
        gotop(12,15);
        printf("Enter Branch.");
        gets(std.branch);
@@ -214,24 +213,24 @@ while(fread(&std,sizeof(std),1,f)==1){
     if(strcmp(sname,std.name)==0){
  
         gotop(12,7);
-       printf("Enter Name :");
+        printf("Enter Name :");
         gets(std.name);
-       gotop(12,9);
-       printf("Enter Mobile Number.");
+        gotop(12,9);
+        printf("Enter Mobile Number.");
         gets(std.mobile);
         gotop(12,11);
-       printf("Enter Roll NO.");
+        printf("Enter Roll NO.");
         scanf("%d",&std.roll);
         fflush(stdin);
         gotop(12,13);
-       printf("Enter Course .");
+        printf("Enter Course .");
         gets(std.course);
-       gotop(12,15);
-       printf("Enter Branch.");
-       gets(std.branch);
-       fseek(f,-sizeof(std),SEEK_CUR);
-       fwrite(&std,sizeof(std),1,f);
-       break;
+        gotop(12,15);
+        printf("Enter Branch.");
+        gets(std.branch);
+        fseek(f,-sizeof(std),SEEK_CUR);
+        fwrite(&std,sizeof(std),1,f);
+        break;
        }
 }
     fclose(f);
@@ -288,7 +287,8 @@ void display(){
     gotop(12,5);
     printf("<<====STUDENT RECORD====>>");
     gotop(12,8);
-    printf("S.No\t||\tName of Student\t||  Mobile No.\t\t||Roll No.||Course||Branch");
+   
+    printf("S.No   |Name of Student       |Mobile No.  |Roll No. |Course      |Branch      ");
     gotop(12,9);
     printf("=====================================================================================");
     f = fopen("record.txt","rb+");
